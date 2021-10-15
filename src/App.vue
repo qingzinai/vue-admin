1e683f6d82e84e34926cbf27d3276fdf10b28d3c
wangchong <ex_chong.wang@tcl.com>
2021-10-15 17:53:55 +0800

    添加vue-router 和 使用@替换引入路径 :sparkles:

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,14 @@
 <template>
-  <input type="text" v-model="newToDoText">
-  <button @click="addNewTodo">添加列表</button>
-  <TodoList v-for="(todo,index) of todos"  :key="todo.id" :title="todo.title" @remove="todos.splice(index,1)" />
-  <iframe src="http://c.biancheng.net/view/746.html" height="500" width="500"></iframe>
+  <RouterView />
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, reactive } from "vue";
-import TodoList from "./views/todoList.vue";
+import { defineComponent, ref, reactive } from 'vue'
 
 export default defineComponent({
-  name: "App",
-  components: {
-    TodoList,
-  },
-  setup() {
-    const newToDoText = ref<string|number>("");
-    const todos:any = reactive([
-      {id: 1,title: "Do the dishes",}, 
-      {id: 2,title: "Take out the trash",},
-      {id: 3,title: "Mow the lawn",},
-    ]);
-    const nextTodoId=ref<number>(4)
-    const addNewTodo=()=>{
-      todos.push({
-        id:nextTodoId.value++,
-        title:newToDoText.value
-      })
-      console.log(todos)
-    }
-    return {
-      newToDoText,
-      todos,
-      addNewTodo
-    };
-  },
-});
+  name: 'App',
+  components: {}
+})
 </script>
 
-<style>
-</style>
+<style></style>
