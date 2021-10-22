317e60f8c4faf2a4272d954560cb404683d3078b
wangchong <ex_chong.wang@tcl.com>
2021-10-22 15:04:45 +0800

    提交vuex-next

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from '@/App.vue'
 import router from '@/router'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
-
+import Store, { key } from '@/store/index'
 import '@/mock/mock'
 
-createApp(App).use(router).use(Antd).mount('#app')
+createApp(App).use(router).use(Antd).use(Store, key).mount('#app')
